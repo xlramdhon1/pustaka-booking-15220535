2924f82b62e358ee517913a01e94b4023fc0b443
xlramdhon1 <ramdhon1.ssss@gmail.com>
2024-10-29 09:49:43 +0700

    + komit kan cuy

--- application/controllers/Laporan.php
+++ application/controllers/Laporan.php
@@ -51,5 +51,51 @@ public function export_excel()
     );
     $this->load->view('buku/export_excel_buku', $data);
 }
-
+public function laporan_pinjam()
+{
+$data['judul'] = 'Laporan Data Peminjaman';
+$data['user'] = $this->ModelUser->cekData(['email' => $this->session->userdata('email')])->row_array();
+$data['laporan'] = $this->db->query("select * from pinjam p,detail_pinjam d,
+buku b,user u where d.id_buku=b.id and p.id_user=u.id
+and p.no_pinjam=d.no_pinjam")->result_array();
+$this->load->view('templates/header', $data);
+$this->load->view('templates/sidebar');
+$this->load->view('templates/topbar', $data);
+$this->load->view('pinjam/laporan-pinjam', $data);
+$this->load->view('templates/footer');
+}
+public function cetak_laporan_pinjam()
+{
+$data['laporan'] = $this->db->query("select * from pinjam p,detail_pinjam d,
+buku b,user u where d.id_buku=b.id and p.id_user=u.id
+and p.no_pinjam=d.no_pinjam")->result_array();
+$this->load->view('pinjam/laporan-print-pinjam', $data);
+}
+public function laporan_pinjam_pdf()
+{
+{
+$this->load->library('dompdf_gen');
+$data['laporan'] = $this->db->query("select * from pinjam p,detail_pinjam d,
+buku b,user u where d.id_buku=b.id and p.id_user=u.id
+and p.no_pinjam=d.no_pinjam")->result_array();
+$this->load->view('pinjam/laporan-pdf-pinjam', $data);
+$paper_size = 'A4'; // ukuran kertas
+$orientation = 'landscape'; //tipe format kertas potrait atau landscape
+$html = $this->output->get_output();
+$this->dompdf->set_paper($paper_size, $orientation);
+//Convert to PDF
+$this->dompdf->load_html($html);
+$this->dompdf->render();
+$this->dompdf->stream("laporan data peminjaman.pdf", array('Attachment' => 0));
+// nama file pdf yang di hasilkan
+}
+}
+public function export_excel_pinjam()
+{
+$data = array( 'title' => 'Laporan Data Peminjaman Buku',
+'laporan' => $this->db->query("select * from pinjam p,detail_pinjam d,
+buku b,user u where d.id_buku=b.id and p.id_user=u.id
+and p.no_pinjam=d.no_pinjam")->result_array());
+$this->load->view('pinjam/export-excel-pinjam', $data);
+}
 }
